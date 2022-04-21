a869d5cbbbc37890495b8a13e7112a9b6da9a156
Juan Trentino <juantrentino@gmail.com>
2022-04-20 22:47:20 -0300

    carrito y caja contenedora - Trentino

---
+++ proyecto-trentino/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './cartWidget.css';
+const CartWidget = () => {
+    return (
+        
+        <img id='carrito' src="https://www.seekpng.com/png/detail/986-9868123_png-file-icono-carrito-de-compras.png" alt="Png File - Icono Carrito De Compras@seekpng.com"/>
+        
+    );
+};
+
+export default CartWidget;
--- proyecto-trentino/src/components/NavBar/NavBar.jsx
+++ proyecto-trentino/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './NavBar.css';
+import CartWidget from '../CartWidget/CartWidget.jsx';
 function NavBar(props) {
     return (
         <div>
             <h1>Todo a Medias</h1>
-            <ul className='header'>
+            <ul className='cabecera'>
             
                 <a href='#'>Soquetes</a>
                 <a href='#'>Media caña</a>
                 <a href='#'>Precios</a>
+                
             </ul>
+            <CartWidget />
         </div>
     );
 }
